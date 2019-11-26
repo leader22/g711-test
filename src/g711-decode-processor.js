0b4748205fcaba10341a667748817e788a5a4b14
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-11-26 15:38:29 +0900

    Cosme

--- src/g711-decode-processor.js
+++ src/g711-decode-processor.js
@@ -7,22 +7,16 @@ class G711DecodeProcessor extends AudioWorkletProcessor {
   process(inputs, outputs) {
     const input = inputs[0];
     const output = outputs[0];
-    // mu is defined as 255: https://ja.wikipedia.org/wiki/%CE%9C-law%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
-    const mu = 255.0;
-    const inv_mu = 1.0 / mu;
-
-    if(this.prev_value.length === 0) {
-      this.prev_value = (new Array(inputs.length)).fill(-10.0);
-    }
 
     for (let channel = 0; channel < output.length; ++channel) {
       for (let i = 0; i < input[channel].length / 2; ++i) {
-        output[channel][i*2 + 1] = dec_u_law(input[channel][i], mu, inv_mu);
+        output[channel][i*2 + 1] = dec_u_law(input[channel][i]);
+
         if(i > 0) {
           output[channel][i*2] = (output[channel][i*2 - 1] / 2.0 + output[channel][i*2 + 1] / 2.0)
         } else {
           // Prevent fft nodes from crashing
-          if(this.prev_value[channel] > -1.0) {
+          if (this.prev_value[channel] > -1.0) {
             output[channel][0] = (this.prev_value[channel] / 2.0 + output[channel][1] / 2.0)
           }
         }
@@ -35,11 +29,12 @@ class G711DecodeProcessor extends AudioWorkletProcessor {
   }
 }
 
-// logic is from here: https://ja.wikipedia.org/wiki/%CE%9C-law%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
-// 1 / mu is a fixed value, so calc it in advance as inv_mu
-function dec_u_law(y, mu, inv_mu) {
-  let sign_y = Math.sign(y);
-  let abs_y = Math.abs(y);
+// mu is defined as 255: https://ja.wikipedia.org/wiki/%CE%9C-law%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
+const mu = 255.0;
+const inv_mu = 1.0 / mu;
+function dec_u_law(y) {
+  const sign_y = Math.sign(y);
+  const abs_y = Math.abs(y);
   return sign_y * inv_mu * (Math.pow(1 + mu, abs_y) - 1);
 }
 
--- src/g711-encode-processor.js
+++ src/g711-encode-processor.js
@@ -2,13 +2,10 @@ class G711EncodeProcessor extends AudioWorkletProcessor {
   process(inputs, outputs) {
     const input = inputs[0];
     const output = outputs[0];
-    // mu is defined as 255: https://ja.wikipedia.org/wiki/%CE%9C-law%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
-    const mu = 255.0;
-    const ln_mu = Math.log(1 + mu);
 
     for (let channel = 0; channel < output.length; ++channel) {
       for (let i = 0; i < input[channel].length / 2; ++i) {
-        output[channel][i] = enc_u_law(input[channel][i*2], mu, ln_mu);
+        output[channel][i] = enc_u_law(input[channel][i*2]);
       }
     }
 
@@ -16,11 +13,12 @@ class G711EncodeProcessor extends AudioWorkletProcessor {
   }
 }
 
-// logic is from here: https://ja.wikipedia.org/wiki/%CE%9C-law%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
-// log(1 + mu) is a fixed value, so calc it in advance as ln_mu
-function enc_u_law(x, mu, ln_mu) {
-  let sign_x = Math.sign(x);
-  let abs_x = Math.abs(x);
+// mu is defined as 255: https://ja.wikipedia.org/wiki/%CE%9C-law%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
+const mu = 255.0;
+const ln_mu = Math.log(1 + mu);
+function enc_u_law(x) {
+  const sign_x = Math.sign(x);
+  const abs_x = Math.abs(x);
   return sign_x * Math.log(1 + mu * abs_x) / ln_mu;
 }
 
