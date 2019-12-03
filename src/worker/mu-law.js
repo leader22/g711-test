e5c8579d622f460061052453916b8da0cb59e98b
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-12-03 19:21:43 +0900

    Refactor

--- src/worker/mu-law.js
+++ src/worker/mu-law.js
@@ -4,15 +4,11 @@ const INV_MU = 1.0 / MU;
 const LN_MU = Math.log(1 + MU);
 
 function encode(x) {
-  const signX = Math.sign(x);
-  const absX = Math.abs(x);
-  return (signX * Math.log(1 + MU * absX)) / LN_MU;
+  return (Math.sign(x) * Math.log(1 + MU * Math.abs(x))) / LN_MU;
 }
 
 function decode(y) {
-  const signY = Math.sign(y);
-  const absY = Math.abs(y);
-  return signY * INV_MU * (Math.pow(1 + MU, absY) - 1);
+  return Math.sign(y) * INV_MU * (Math.pow(1 + MU, Math.abs(y)) - 1);
 }
 
 self.muLaw = { encode, decode };
