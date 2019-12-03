84b51fc3e0b2a0d0acff4d0724469532556daffe
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-12-03 16:50:30 +0900

    Cosme

--- src/recver.js
+++ src/recver.js
@@ -3,15 +3,13 @@ import visualizeStream from "./visualize-stream.js";
 import Player from "./player.js";
 
 export async function recvBypass($canvas) {
-  const Recver = Comlink.wrap(new Worker("./src/worker/recver.js"));
-  const recver = await new Recver("audio");
-
   const audioContext = new AudioContext();
   console.log("recver sampleRate:", audioContext.sampleRate);
 
   const player = new Player(audioContext);
-  window.player = player;
 
+  const Recver = Comlink.wrap(new Worker("./src/worker/recver.js"));
+  const recver = await new Recver("audio");
   recver.onmessage = Comlink.proxy(data => player.enqueue(data));
 
   const recvNode = player.startAsNode();
